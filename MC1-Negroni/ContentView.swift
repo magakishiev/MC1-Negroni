acaf7dfdf89d858149154a75f0652af6b0ab65bf
Muhamed Agakishiev <m.agakishiev@icloud.com>
2021-11-11 12:52:34 +0100

    Initial Commit

---
+++ MC1-Negroni/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MC1-Negroni
+//
+//  Created by Muhamed Agakishiev on 11/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MC1-Negroni/MC1_NegroniApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC1_NegroniApp.swift
+//  MC1-Negroni
+//
+//  Created by Muhamed Agakishiev on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MC1_NegroniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
